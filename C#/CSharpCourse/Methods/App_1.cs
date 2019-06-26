948226404828e9113e059aba24029b3b7d939d5b
Oscar Olsson <happybits@gmail.com>
2019-06-26 11:08:54 +0200

    -

---
+++ C#/CSharpCourse/Methods/App_1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CSharpCourse.Methods
+{
+    public class App_1
+    {
+        public void Run()
+        {
+            string response = GetInputFromUser();
+
+            string[] peopleArray = CreateArrayOfPeople(response);
+
+            RespondToUser(peopleArray);
+        }
+
+        private string GetInputFromUser()
+        {
+            Write("Enter a list of names: ");
+            string answer = Console.ReadLine();
+
+            return answer;
+        }
+
+        private string[] CreateArrayOfPeople(string response)
+        {
+            if (string.IsNullOrWhiteSpace(response))
+                return new string[] { };
+
+            string[] list = response.Split(',');
+
+            return list;
+        }
+
+        private void RespondToUser(string[] peopleArray)
+        {
+            if (peopleArray.Length == 0)
+            {
+                WriteGreen("No people in the list");
+                return;
+            }
+
+            foreach (string person in peopleArray)
+            {
+                WriteGreen($"***SUPER-{person.ToUpper()}***");
+            }
+        }
+
+
+        private void WriteGreen(object message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+
+        private void Write(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(text);
+        }
+    }
+}
---
+++ C#/CSharpCourse/Methods/App_1_no_methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSharpCourse.Methods
+{
+    public class App_1_no_methods
+    {
+        public void Run()
+        {
+            // Get list of names from user
+
+            Console.Write("Enter a list of names: ");
+            string listOfNames = Console.ReadLine();
+
+            // Create list
+
+            string[] list = listOfNames?.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Report to user
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (list == null || list.Length == 0)
+            {
+                Console.WriteLine("No people in the list");
+            }
+            else
+            {
+                foreach (string person in list)
+                {
+                    Console.WriteLine($"***SUPER-{person.ToUpper()}***");
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
